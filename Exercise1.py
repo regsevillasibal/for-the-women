6a6e494fa0d6d59f11bb81e8a3b49109873d1014
Reg Sevilla Sibal <57027759+regsevillasibal@users.noreply.github.com>
2019-11-08 22:40:23 +0800

    Delete Exercise1.py

--- Exercise1.py
+++
@@ -1,7 +0,0 @@
-# Exercise 1- given two variables- name and age
-# Use the format () function to create a sentence that reads
-# "hi my name is julie and I am 42 years old"
-name = "Julie"
-age = 42
-sentence = "Hi my name is {} and i am {} years old".format(name,age)
-print(sentence)
